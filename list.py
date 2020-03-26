db75bb8a06bafeba302c7844cce6d6ed6db4d3fb
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-03-26 18:45:36 +0530

    set/list/dictionary practice

---
+++ list.py
@@ -0,0 +1,19 @@
+names1 = ['Amir', 'Barry', 'Chales', 'Dao']
+names2 = names1   #referencing 
+names3 = names1[:] #here data is copying into a new list
+
+names2[0] = 'Alice'
+print(names1)
+print(names2)
+print(names3)
+names3[1] = 'Bob'
+
+sum = 0
+for ls in (names1, names2, names3):
+    if ls[0] == 'Alice':
+        sum += 1
+     #   print(ls[0])
+    if ls[1] == 'Bob':
+        sum += 10
+   # print(ls)
+print(sum)
