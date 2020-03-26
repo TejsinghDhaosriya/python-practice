6a0a89e1704a82db3d5124aaf6934721207c4cd4
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-03-26 19:09:04 +0530

    set/list/dictionary practice

--- stringlower.py
+++ stringlower.py
@@ -2,3 +2,17 @@ names1 = ['Amir', 'Barry', 'Chales', 'Dao']
 names2 = [name.lower() for name in names1]
 print(names2)
 print (names2[2][0])
+
+
+
+
+vec = [2, 4, 6]
+print()
+print([3*x for x in vec if x > 3])
+
+
+#vec = [2, 4, 6]
+
+x1 = [x, x**2 for x in vec]
+
+print (x1)
