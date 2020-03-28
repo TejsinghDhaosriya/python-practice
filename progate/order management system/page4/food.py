d3189e67efc6372046ed480692bba1aa8a322940
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-03-28 23:23:59 +0530

    order management system

---
+++ progate/order management system/page4/food.py
@@ -0,0 +1,8 @@
+from menu_item import MenuItem
+
+class Food(MenuItem):
+    # Define the calorie_info method
+    def calorie_info(self) :
+        print('kcal: ' + str(self.calorie_count))
+        
+    
