9d18f27ff724c8cfbcb4a4984f996f05b6e3e8ec
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-03-29 21:30:35 +0530

    Rock Paper Scissors

---
+++ progate/Rock Paper Scissors/page4/script.py
@@ -0,0 +1,11 @@
+# Add a parameter to print_hand
+def print_hand(hand,name):
+    # Change the output to '___ picked: ___'
+    print('You picked: ' + hand)
+    print(name+'picked:'+hand)
+
+# Add a second argument to print_hand
+print_hand('Rock','Ken the Ninja')
+
+# Add a second argument to print_hand
+print_hand('Paper','Computer')
