394a2a6d07a2465964bb462abe077a38692fc372
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-04-15 19:28:23 +0530

    Conditional

--- jumpstart/loop11.py
+++ jumpstart/loop11.py
@@ -1,4 +1,9 @@
-list1 =[ ['ram','shayam','ghanshyam'],['ram','shayam','ghanshyam',],["apple", "banana", "cherry"],[  "kiwi", "melon", "mango"]]
-
-for list ,l,s in list1:
-    print(list, l, s)
+list1 =[ ['ram','shayam','ghanshyam'],['ram','shayam','ghanshyam',],["apple", "banana", "cherry"],[  "kiwi", "melon", "mango"],[1, 2, 3]]
+dict1 =[ ['ram','ghanshyam'],['ram','shayam'],["apple", "banana"],[  "kiwi", "melon" ]]
+list1  = [1,2,3,4]
+dict2 = dict(dict1)
+for list12l in dict2:
+    print(list12l )
+for list12l in list1:
+    if str(list12l).isnumeric() and list12l >1:
+        print(list12l )
