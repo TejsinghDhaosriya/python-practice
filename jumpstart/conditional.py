35a35a28570a49f98fd4b3d3a1fd3614347c035f
TejsinghDhaosriya <tejsinghdhaosriya@gmail.com>
2020-04-15 18:59:18 +0530

    Conditional

---
+++ jumpstart/conditional.py
@@ -0,0 +1,9 @@
+age = int(input("What is your age?"))
+if age>18:
+    print("You can drive")
+
+elif age==18:
+    print("We cannot decide you can drive or not")
+
+else :
+    print("You cannot drive !")
